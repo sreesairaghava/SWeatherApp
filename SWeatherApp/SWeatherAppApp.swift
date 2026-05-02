f5e90c3368f5964fd1a20e9e849e540c73810fde
Sree Sai Raghava <raghava.rym@gmail.com>
2026-05-02 17:19:01 +0530

    Initial Commit

---
+++ SWeatherApp/SWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWeatherAppApp.swift
+//  SWeatherApp
+//
+//  Created by Sree Sai Raghava Dandu on 02/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
